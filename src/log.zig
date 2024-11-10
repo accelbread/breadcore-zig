dcb6936c641f6a951e468e2c4b1cfb3948717274
Archit Gupta <archit@accelbread.com>
2024-11-10 00:49:02 -0800

    Fix enum literal casing

--- src/log.zig
+++ src/log.zig
@@ -20,7 +20,7 @@ const std = @import("std");
 
 pub fn logFn(
     comptime level: std.log.Level,
-    comptime scope: @TypeOf(.EnumLiteral),
+    comptime scope: @TypeOf(.enum_literal),
     comptime format: []const u8,
     args: anytype,
 ) void {
